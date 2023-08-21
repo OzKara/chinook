493f126b2429898fd7354ddf652a84d347f50f38
OzKara <ozankarapri@gmail.com>
2023-08-21 14:16:28 +0200

    Initilize Spring Boot, Chinook project

---
+++ src/main/java/no/accelerate/chinook/ChinookApplication.java
@@ -0,0 +1,14 @@
+package no.accelerate.chinook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChinookApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ChinookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/no/accelerate/chinook/ChinookApplicationTests.java
@@ -0,0 +1,13 @@
+package no.accelerate.chinook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChinookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
