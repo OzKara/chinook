5caaae2d93d77cf41313785d8fd4570e52da9963
MichalPajestka <001michal002@gmail.com>
2023-08-23 14:22:58 +0200

    Fixed the CustomerGenre model

--- src/main/java/no/accelerate/chinook/models/CustomerGenre.java
+++ src/main/java/no/accelerate/chinook/models/CustomerGenre.java
@@ -5,55 +5,21 @@ import java.util.List;
 public class CustomerGenre {
 
     private Long id;
-    private String firstName;
-    private String lastName;
-    private List<String> popularGenres;
+    private String genre;
+    private int count;
 
-    public CustomerGenre(Long id, String firstName, String lastName, List<String> popularGenres) {
+    public CustomerGenre(Long id, String genre, int count) {
         this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.popularGenres = popularGenres;
+        this.genre = genre;
+        this.count = count;
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public List<String> getPopularGenres() {
-        return popularGenres;
-    }
-
-    public void setPopularGenres(List<String> popularGenres) {
-        this.popularGenres = popularGenres;
-    }
 
     public String toString() {
         return "CustomerGenre{" +
                 "id=" + id +
-                "firstName=" + firstName +  '\'' +
-                "lastName=" + lastName + '\'' +
-                "popularGenres=" + popularGenres +
+                "genre=" + genre +  '\'' +
+                "count=" + count +
                 '}';
 
     }
