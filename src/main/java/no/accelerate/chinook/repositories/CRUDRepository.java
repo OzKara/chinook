47fc4441393a80595ad437d1cf7da0a995b5482f
MichalPajestka <001michal002@gmail.com>
2023-08-24 15:24:20 +0200

    Added JavaDocs to methods

--- src/main/java/no/accelerate/chinook/repositories/CRUDRepository.java
+++ src/main/java/no/accelerate/chinook/repositories/CRUDRepository.java
@@ -2,12 +2,58 @@ package no.accelerate.chinook.repositories;
 
 import java.util.List;
 
+/**
+ * A repository interface providing basic CRUD operations for entities.
+ *
+ * @param <T> The type of the entity.
+ * @param <U> The type of the entity's ID.
+ */
 public interface CRUDRepository<T, U> {
 
+    /**
+     * Retrieves all customers.
+     *
+     * @return A list containing all customers.
+     */
     List<T> findAll();
+
+    /**
+     * Retrieves a customer by ID.
+     *
+     * @param id The ID of the customer
+     * @return The retrieved customer, or null if not found.
+     */
     T findById(U id);
+
+    /**
+     * Inserts a new customer into the repository.
+     *
+     * @param object The customer to insert.
+     * @return The number of affected rows.
+     */
     int insert(T object);
+
+    /**
+     * Updates an existing customer in the repository.
+     *
+     * @param object The entity to update.
+     * @return The number of affected rows.
+     */
     int update(T object);
+
+    /**
+     * Deletes a customer.
+     *
+     * @param object The customer to delete.
+     * @return The number of affected rows.
+     */
     int delete(T object);
+
+    /**
+     * Deletes a customer by its ID.
+     *
+     * @param id The ID of the customer to delete.
+     * @return The number of affected rows.
+     */
     int deleteById(U id);
 }
--- src/main/java/no/accelerate/chinook/repositories/CustomerRepository.java
+++ src/main/java/no/accelerate/chinook/repositories/CustomerRepository.java
@@ -9,15 +9,44 @@ import java.util.List;
 
 public interface CustomerRepository extends CRUDRepository<Customer, Long> {
 
+    /**
+     * Finds customers by their name.
+     *
+     * @param name The name of the customer to search for.
+     * @return A list of customers matching the provided name.
+     */
     List<Customer> findByName(String name);
 
+    /**
+     * Retrieves a subset of customers with specified limits.
+     *
+     * @param limit The maximum number of customers to retrieve.
+     * @param offset The starting index of customers to retrieve.
+     * @return A list of customers within the specified limit and offset.
+     */
     List<Customer> getCustomerSubset(int limit, int offset);
 
+    /**
+     * Finds the customer who has spent the most money.
+     *
+     * @return The customer with the highest total spent.
+     */
     CustomerSpender findHighestSpender();
 
+    /**
+     * Finds the most popular genres of tracks purchased by a customer.
+     *
+     * @param customerId The ID of the customer.
+     * @return A list of the most popular genres.
+     */
     List<CustomerGenre> findMostPopularGenres(Long customerId);
 
 
+    /**
+     * Retrieves the country with the highest number of customers.
+     *
+     * @return The country with the most customers.
+     */
     CustomerCountry getCountryWithMostCustomers();
 
 }
